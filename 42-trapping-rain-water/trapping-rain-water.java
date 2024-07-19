5e5d1efe91ca5a2c6b826ece527512fa1a83193f
ananyasinha1910 <115775255+ananyasinha1910@users.noreply.github.com>
2024-07-19 18:10:48 +0530

    Time: 126 ms (6.19%) | Memory: 46.4 MB (29.30%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,27 +1,34 @@
 class Solution {
     public int trap(int[] height) {
-    int left = 0, right = height.length - 1;
-    int leftMax = 0, rightMax = 0;
-    int trappedWater = 0;
+        int[] maxL = new int[height.length];
+        int[] maxR = new int[height.length];
+        int maxLnum = 0;
+        int maxRnum = 0;
+        int totalwater = 0;
 
-    while (left < right) {
-        if (height[left] < height[right]) {
-            if (height[left] > leftMax) {
-                leftMax = height[left];
-            } else {
-                trappedWater += leftMax - height[left];
-            }
-            left++;
-        } else {
-            if (height[right] > rightMax) {
-                rightMax = height[right];
-            } else {
-                trappedWater += rightMax - height[right];
-            }
-            right--;
+        for(int i = 0; i < height.length; i++){
+            maxLnum = Math.max(height[i], maxLnum);
+            maxL[i] = maxLnum;
         }
-    }
+        System.out.println(Arrays.toString(maxL));
 
-    return trappedWater;
+        for(int j = height.length - 1; j >= 0; j--){
+            maxRnum = Math.max(height[j], maxRnum);
+            maxR[j] = maxRnum;
+        }
+
+
+        System.out.println(Arrays.toString(maxR));
+        
+
+        for(int k = 0; k < height.length; k++){
+            int maxmin = Math.min(maxR[k], maxL[k]);
+            //System.out.print( maxmin + " ");
+            //System.out.println();
+            int water = maxmin - height[k];
+            System.out.print( water + " ");
+            totalwater = totalwater + water;
+        }
+        return totalwater;
     }
 }
